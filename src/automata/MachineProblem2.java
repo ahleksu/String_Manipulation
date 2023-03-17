3934c6f84cce9394645eb486c45c7af6979040c3
ahleksu <roblesjohnalex18@gmail.com>
2023-03-17 09:16:35 +0800

    Applied changes

--- src/automata/MachineProblem2.java
+++ src/automata/MachineProblem2.java
@@ -17,18 +17,20 @@ public class MachineProblem2 {
 	        int len2 = string2.length();
 
 	        // create palindrome versions of the strings
-	        String palindrome1 = string1 + new StringBuilder(string1).reverse().toString();
-	        String palindrome2 = string2 + new StringBuilder(string2).reverse().toString();
+	        String palindrome1 = new StringBuilder(string1).reverse().toString();
+	        String palindrome2 = new StringBuilder(string2).reverse().toString();
 
 	        // concatenate the strings
-	        String concatenation = string1 + string2;
+	        String concatenation1 = string1 + string2;
+	        String concatenation2 = string2 + string1;
 
 	        // display the results
-	        System.out.println("The length of the first string is: " + len1);
-	        System.out.println("The length of the second string is: " + len2);
-	        System.out.println("The palindrome of the first string is: " + palindrome1);
-	        System.out.println("The palindrome of the second string is: " + palindrome2);
-	        System.out.println("The concatenation of the two strings is: " + concatenation);
+	        System.out.println("The length of the first string "+ string1 + " is: " + len1);
+	        System.out.println("The length of the second string "+ string2 + " is: " + len2);
+	        System.out.println("The palindrome of the first string" + string1 + " is: " + palindrome1);
+	        System.out.println("The palindrome of the second string"+ string2 + " is: " + palindrome2);
+	        System.out.println("The concatenation of string 1 and 2 is: " + concatenation1);
+	        System.out.println("The concatenation of string 2 and 1 is: " + concatenation2);
 
 	        sc.close();
 	    }
